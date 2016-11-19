5c377d99b2a0100999448cb1405c316344835cc9
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-11-19 14:20:53 +0000

    setting up frontend pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,51 @@
 var gulp         = require("gulp"),
     sass         = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
+    hash         = require("gulp-hash"),
+    del          = require("del"),
     srcRootDir   = "src/",
     destRootDir  = "static/",
     srcCssDir    = srcRootDir + "css/",
     srcJsDir     = srcRootDir + "js/",
-    destCssDir   = destRootDir + "css/",
-    destJsDir    = destRootDir + "js/";
+    srcCss       = srcCssDir + "**/*.scss",
+    srcJs        = srcJsDir + "**/*.js",
+    destCssDir   = destRootDir + "css",
+    destJsDir    = destRootDir + "js";
 
 gulp.task("js", function() {
+    del.sync([destJsDir]);
+
     gulp
-        .src(srcJsDir + "**/*.js")
+        .src(srcJs)
+        .pipe(hash())
         .pipe(gulp.dest(destJsDir))
+        .pipe(hash.manifest("hash.json"))
+        .pipe(gulp.dest("data/"));
 });
 
 gulp.task("css", function() {
+    del.sync([destCssDir]);
+
     gulp
-        .src(srcCssDir + "**/*.scss")
+        .src(srcCss)
         .pipe(sass({
             outputStyle: "compressed"
         }))
         .pipe(autoprefixer({
             browsers: ["last 20 versions"]
         }))
+        .pipe(hash())
         .pipe(gulp.dest(destCssDir))
+        .pipe(hash.manifest("hash.json"))
+        .pipe(gulp.dest("data/"));
 });
 
+gulp.task("clean", function(){
+    del([destCssDir, destJsDir, "data"]);
+})
+
 gulp.task("watch", ["css", "js"], function() {
-    gulp.watch([srcCssDir, srcJsDir], ["css", "js"])
+    gulp.watch([srcCss, srcJs], ["css", "js"]);
 });
 
 gulp.task("default", ["watch"]);
